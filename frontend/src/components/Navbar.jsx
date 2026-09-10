70ca4b38f627c2e08faa58d4de4de2d8b6cef2d4
jackharsh8084 <harshitaman58@g-mail.com>
2026-09-10 23:27:01 +0530

    completed frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import home from '../assets/home.svg'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="w-full bg-black text-white border-b border-gray-200">
+       <nav className="flex justify-between items-center py-4">
+            <div className="ml-7 text-xl font-bold">Umeed</div>
+            <div>
+                <ul className="flex list-none mr-12 space-x-6 items-center">
+                    <NavLink to={'/'}>
+                      <li className='my-2.5 mx-1.25'>
+                        <img title="Home" src={home} className="w-6 h-6" alt="Home"/>
+                      </li>
+                    </NavLink>
+                    <NavLink to={'/login'}>
+                      <li className='my-2.5 mx-1.25'>
+                        <img title="Login" src={home} className="w-6 h-6" alt="Login"/>
+                      </li>
+                    </NavLink>
+                    <NavLink to={'/questions'}>
+                      <li className='my-2.5 mx-1.25'>
+                        <img title="Questions" src={home} className="w-6 h-6" alt="Questions"/>
+                      </li>
+                    </NavLink>
+                    <NavLink to={'/analysis'}>
+                      <li className='my-2.5 mx-1.25'>
+                        <img title="Analysis" src={home} className="w-6 h-6" alt="Analysis"/>
+                      </li>
+                    </NavLink>
+                </ul>
+            </div>
+        </nav>
+    </div>
   )
 }
 
