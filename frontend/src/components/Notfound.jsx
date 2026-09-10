67219489f282c63aaa37e1b6e7458c61a6d76bab
jackharsh8084 <harshitaman58@g-mail.com>
2026-09-10 22:47:21 +0530

    corrected frontend

--- frontend/src/components/Notfound.jsx
+++ frontend/src/components/Notfound.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Body from './Body'
 
 const Notfound = () => {
   return (
     <div>
-        <div className="flex flex-col items-center justify-center h-{30vh} w-full">
-            <h2 className='flex justify-center items-center font-{50px} bg-white text-black '>404 | Page not found</h2>
+        <div className="flex flex-col items-center justify-center h-[30vh] w-full text-black">
+            <h2 className='flex justify-center items-center text-[50px] bg-white text-black'>
+              404 | Page not found
+            </h2>
         </div>
     </div>
   )
